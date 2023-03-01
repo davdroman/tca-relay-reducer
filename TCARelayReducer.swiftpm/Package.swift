8cad0664c568f621af6ffd4d731cc0407b62850b
David Roman <2538074+davdroman@users.noreply.github.com>
2023-03-01 12:01:37 +0000

    update deps

--- TCARelayReducer.swiftpm/Package.swift
+++ TCARelayReducer.swiftpm/Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/davdroman/swift-composable-architecture", .branch("scope-local-state-action")),
+        .package(url: "https://github.com/davdroman/swift-composable-architecture", .branch("navigation-relay")),
         .package(url: "https://github.com/davdroman/swiftui-navigation-transitions", "0.7.2"..<"1.0.0"),
         .package(url: "https://github.com/apple/swift-async-algorithms", "0.0.4"..<"1.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-nonempty", "0.4.0"..<"1.0.0"),
